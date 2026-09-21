880a6571a92ed83665b2b35b0173b1f6ed010246
mohamed-bouth <mohamedbouth87@gmail.com>
2026-09-21 11:14:22 +0000

    Étape 2 — Runtime : navigateur vs Node.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("learning-report-cli");
+// Etap 1 :
+
+// console.log("learning-report-cli");
+
+// Etap 2 :
+
+console.log(typeof window);
+console.log(typeof document);
+console.log(typeof process);
